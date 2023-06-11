edf9cf36c10ae93e72a04f9e4f6ac77f996c1e8b
bbiswal <bismaykumar.biswal@yahooinc.com>
2023-06-11 08:38:58 +0530

    fix

--- src/data/index.ts
+++ src/data/index.ts
@@ -36,7 +36,7 @@ export class DataStore {
    }
 
    public async listBookmarks(): Promise<any[]> {
-    return await bookmarks.list();
+    return await bookmarks;
    }
 
    public async deleteBookmark(bookmarkId:String) : Promise<void> {
